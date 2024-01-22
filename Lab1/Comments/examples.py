23c603772351bea67b74850e25dc280e4c8dfdaa
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-22 12:57:46 +0600

    Initial commit

--- Lab1/Comments/examples.py
+++ Lab1/Comments/examples.py
@@ -1,19 +1,19 @@
-#This is a comment
+#This is a comment - ex1
 print("Hello, World!")
 
-print("Hello, World!") #This is a comment
+print("Hello, World!") #This is a comment - ex2
 
-#print("Hello, World!")
+#print("Hello, World!") - ex3
 print("Cheers, Mate!")
 
 #This is a comment
 #written in
-#more than just one line
+#more than just one line - ex4
 print("Hello, World!")
 
 """
 This is a comment
 written in
-more than just one line
+more than just one line - ex5
 """
 print("Hello, World!")
--- Lab1/Comments/tasks.py
+++ Lab1/Comments/tasks.py
@@ -1,11 +1,7 @@
-#task1
-
-#This is a comment
-
-#task 2
+#This is a comment - task1
 
 """
 This is a comment
 written in 
-more than just one line
+more than just one line - task2
 """
