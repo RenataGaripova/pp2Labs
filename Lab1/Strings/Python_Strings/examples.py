aa4071d0d9327e62a51bf09e5a43800f4fade4be
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-22 13:28:21 +0600

    new commit

---
+++ Lab1/Strings/Python_Strings/examples.py
@@ -0,0 +1,68 @@
+#ex1
+
+print("Hello")
+print('Hello')
+
+
+#ex2
+
+a = "Hello"
+print(a)
+
+#ex3
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+#ex4
+
+a = "Hello, World!"
+print(a[1])
+
+
+#ex5
+
+for x in "banana":
+  print(x)
+
+
+#ex6
+
+a = "Hello, World!"
+print(len(a))
+
+
+#ex7
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+#ex8
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+#ex9
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+#ex9
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
