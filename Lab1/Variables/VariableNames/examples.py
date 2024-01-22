f1ad00e8e6a39b09fa55e15865610f8ae17ea88d
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-22 14:16:01 +0600

    new commit

---
+++ Lab1/Variables/VariableNames/examples.py
@@ -0,0 +1,22 @@
+#ex1
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+#ex2
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+
+#ex3
+
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
