61d870aae4adb59e837a89a716b2de13cad51dd7
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-26 09:48:52 +0600

    initial commit

---
+++ Lab2/Python_Lists/Lists/examples.py
@@ -0,0 +1,47 @@
+#ex1
+
+mylist = ["apple", "banana", "cherry"]
+
+
+#ex2
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist) #output: ['apple', 'banana', 'cherry']
+
+
+#ex3
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist) #output: ['apple', 'banana', 'cherry', 'apple', 'cherry']
+
+
+#ex4
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) #output: 3
+
+
+#ex5
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+
+#ex6
+
+list1 = ["abc", 34, True, 40, "male"]
+
+
+#ex7
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist)) #output: <class 'list'>
+
+
+#ex8
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist) #output: ['apple', 'banana', 'cherry']
+
+
