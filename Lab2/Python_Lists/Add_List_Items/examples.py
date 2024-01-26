7f4d512668599c22e2abadc6e219989057f5a759
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-26 10:20:42 +0600

    Initial commit

---
+++ Lab2/Python_Lists/Add_List_Items/examples.py
@@ -0,0 +1,28 @@
+#ex1
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist) #output: ['apple', 'banana', 'cherry', 'orange']
+
+
+#ex2
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist) #output: ['apple', 'orange', 'banana', 'cherry']
+
+
+#ex3
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist) #output: ['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+
+
+#ex4
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
