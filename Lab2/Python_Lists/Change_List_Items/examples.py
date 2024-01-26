b9b4bab9dff3bfdd898e97b7997249cb2a89a7ab
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-26 10:10:45 +0600

    Initial commit"
    q
    
    "

---
+++ Lab2/Python_Lists/Change_List_Items/examples.py
@@ -0,0 +1,27 @@
+#ex1
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist) #output: ['apple', 'blackcurrant', 'cherry']
+
+
+#ex2
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist) #output: ['apple', 'blackcurrant', 'watermelon' 'orange', 'kiwi', 'mango']
+
+
+#ex8
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist) #['apple', 'blackcurrant', 'watermelon', 'cherry']
+
+
+#ex9
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist) #output: ['apple', 'watermelon']
+
