c5242fdff6fd708ad4dee9f7cbb0228deffcbb1b
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-28 21:46:45 +0600

    Initial commit

---
+++ Lab2/Python_Tuples/Tuples/examples.py
@@ -0,0 +1,10 @@
+#ex1
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple) #output: ('apple', 'banana', 'cherry')
+
+
+#ex2
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple) #output: ('apple', 'banana', 'cherry', 'apple', 'cherry')
