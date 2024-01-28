e9845eb616c1f83c8e08a4b90c561e78d06fed32
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-28 21:33:48 +0600

    Initial commit

---
+++ Lab2/Python_Lists/Copy_Lists/examples.py
@@ -0,0 +1,12 @@
+#ex1
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist) #output: ['apple', 'banana', 'cherry']
+
+
+#ex2
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist) #output: ['apple', 'banana', 'cherry']
