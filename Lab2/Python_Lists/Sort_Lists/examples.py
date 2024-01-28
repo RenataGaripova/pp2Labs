2347d988aa717ab821174a3d2951683545636f7d
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-28 21:30:50 +0600

    Initial commit

---
+++ Lab2/Python_Lists/Sort_Lists/examples.py
@@ -0,0 +1,27 @@
+#ex1
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist) #output: ['banana', 'kiwi', 'mango', 'orange', 'pineapple']
+
+
+#ex2
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist) #output:
+[23, 50, 65, 82, 100]
+
+
+#ex3
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist) #output: ['pineapple', 'orange', 'mango', 'kiwi', 'banana']
+
+
+#ex4
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist) #output: [100, 82, 65, 50, 23]
