c1c1b114cf7287ba3ebf6a896136667045a17bd3
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-29 16:50:25 +0600

    Initial commit

---
+++ Lab2/Python_Sets/Access_Set_Items/examples.py
@@ -0,0 +1,15 @@
+#ex1
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x) #output: apple
+           #banana
+           #cherry
+
+
+#ex2
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset) #output: True
