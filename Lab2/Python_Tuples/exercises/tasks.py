f3fe5372b5af4653e9da1a2ef27bbebd59ae380d
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-29 16:40:15 +0600

    Initial commit

---
+++ Lab2/Python_Tuples/exercises/tasks.py
@@ -0,0 +1,22 @@
+#task1
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0]) #output: apple
+
+
+#task2
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits)) #output: 3
+
+
+#task3
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1]) #output: cherry
+
+
+#task4
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5]) #output: ("cherry", "orange", "kiwi")
