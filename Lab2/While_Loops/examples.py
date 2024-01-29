5a287929e447d2cebf81d689933c4e6adc13f0db
Renata Garipova <rena.garipova.2005@gmail.com>
2024-01-29 18:37:25 +0600

    new commit

--- Lab2/While_Loops/examples.py
+++ Lab2/While_Loops/examples.py
@@ -2,35 +2,35 @@
 
 i = 1
 while i < 6:
-  print(i)
-  i += 1
+    print(i)
+    i += 1
 
 
 #task2
 
 i = 1
 while i < 6:
-  print(i)
-  if i == 3:
-    break
-  i += 1
+    print(i)
+    if i == 3:
+        break
+    i += 1
 
 
 #task3
 
 i = 0
 while i < 6:
-  i += 1
-  if i == 3:
-    continue
-  print(i)
+    i += 1
+    if i == 3:
+        continue
+    print(i)
 
 
 #task4
 
 i = 1
 while i < 6:
-  print(i)
-  i += 1
+    print(i)
+    i += 1
 else:
-  print("i is no longer less than 6")
+    print("i is no longer less than 6")
