f79644fc80bfa1a68f1c5dce4d6cb41870e32c82
Renata Garipova <rena.garipova.2005@gmail.com>
2024-02-07 21:30:28 +0600

    new commit

---
+++ Lab3/functions1/import.py
@@ -0,0 +1,70 @@
+from func1 import *
+
+print(grams_to_ounces(10))
+
+
+from func2 import *
+
+print(convert_temperature(365))
+
+
+from func3 import *
+
+print(solve(35, 94))
+
+
+from func4 import *
+
+a = "1 2 34 56 11 34 567 81 0 3"
+
+print(filter_prime(a))
+
+
+from func5 import *
+
+print(*permutations_string('ABCDE'))
+
+
+from func6 import *
+
+print(reverse_str("We are ready"))
+
+
+from func7 import *
+
+print(has_33([1, 3, 3]))
+print(has_33([1, 3, 1, 3]))
+print(has_33([3, 1, 3]))
+
+
+from func8 import *
+
+print(spy_game([1,2,4,0,0,7,5]))
+print(spy_game([1,0,2,4,0,5,7]))
+print(spy_game([1,7,2,0,4,5,0]))
+
+
+from func9 import *
+
+print(sphere_volume(5))
+
+
+from func10 import *
+
+print(only_unique([1, 11, 45, 67, 67, 67, True, 1, 87]))
+
+
+from func11 import *
+
+print(is_palindrome("madam"))
+print(is_palindrome("kbtu"))
+
+
+from func12 import *
+
+print(print_hyst([4, 1, 10, 8, 5]))
+
+
+from func13 import *
+
+play_game()
