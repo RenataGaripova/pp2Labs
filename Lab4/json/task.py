7c3002073025fddefabec45626b6058f3339e532
Renata Garipova <rena.garipova.2005@gmail.com>
2024-02-13 18:03:57 +0600

    Initial commit

---
+++ Lab4/json/task.py
@@ -0,0 +1,20 @@
+import json
+
+print('Interface Status')
+print('================================================================================')
+print('DN                                                 Description           Speed    MTU')
+print('-------------------------------------------------- --------------------  ------  ------')
+
+
+with open('sample-data.json', 'r') as file:
+    sample_data = json.load(file)
+
+    for object in sample_data['imdata']:
+        attr = object['l1PhysIf']['attributes']
+
+        dn = attr['dn']
+        descr = attr['descr']
+        speed = attr['speed']
+        MTU = attr['mtu']
+
+        print(f'{dn}         {descr}                     {speed}   {MTU}')
